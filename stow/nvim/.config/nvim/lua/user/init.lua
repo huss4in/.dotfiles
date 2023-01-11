8478d45e4006301a89daf86a7e4a45a519013c47
Hussain <huss4in7@outlook.com>
2023-01-11 13:18:13 +0300

    Update nvim config

--- stow/nvim/.config/nvim/lua/user/init.lua
+++ stow/nvim/.config/nvim/lua/user/init.lua
@@ -40,13 +40,14 @@ local config = {
 		},
 		g = {
 			mapleader = " ", -- sets vim.g.mapleader
-			autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+			autoformat_enabled = false, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
 			cmp_enabled = true, -- enable completion at start
 			autopairs_enabled = true, -- enable autopairs at start
 			diagnostics_enabled = true, -- enable diagnostics at start
 			status_diagnostics_enabled = true, -- enable diagnostics in statusline
 			icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
 			ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+			heirline_bufferline = false, -- enable new heirline based bufferline (requires :PackerSync after changing)
 
 			---# Custom #---
 
@@ -296,7 +297,7 @@ local config = {
 		},
 		-- All other entries override the require("<key>").setup({...}) call for default plugins
 		["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
-			config.sources = {}
+			-- config.sources = {}
 			return config -- return final config table
 		end,
 		treesitter = { -- overrides `require("treesitter").setup(...)`
